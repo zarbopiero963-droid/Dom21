126ea5a741fbd46f7b2b7e83e1c2ca2a50aeb5b4
zarbopiero963-droid <zarbopiero963@gmail.com>
2026-02-28 21:57:06 +0100

    Update dom_self_healing.py

--- core/dom_self_healing.py
+++ core/dom_self_healing.py
@@ -1,42 +1,27 @@
-import time
-
 def attempt_heal_selector(executor, broken_selector, context_name):
-    """
-    Tenta di curare un selettore usando il CDP AutoMapper in-place.
-    """
     lock = getattr(executor, "_browser_lock", None)
     lock_acquired = False
     
     try:
-        if lock:
-            lock_acquired = lock.acquire(blocking=True)
+        if lock: lock_acquired = lock.acquire(blocking=True)
             
         page = getattr(executor, "page", None)
-        if not page:
-            return None
+        if not page: return None
             
         cdp = None
         try:
             cdp = page.context.new_cdp_session(page)
             cdp.send("DOM.enable")
-        except Exception:
-            return None
+        except: return None
 
         try:
-            try:
-                resp = cdp.send("DOM.getFlattenedDocument", {"depth": -1, "pierce": True})
-            except Exception:
-                resp = cdp.send("DOM.getFlattenedDocument", {"depth": 4, "pierce": False})
-                
-            nodes = resp.get("nodes", [])
+            try: resp = cdp.send("DOM.getFlattenedDocument", {"depth": -1, "pierce": True})
+            except: resp = cdp.send("DOM.getFlattenedDocument", {"depth": 4, "pierce": False})
             return None
-            
-        except Exception:
-            return None
+        except: return None
         finally:
             if cdp:
                 try: cdp.detach()
                 except: pass
     finally:
-        if lock_acquired and lock:
-            lock.release()
+        if lock_acquired and lock: lock.release()
